82164f80879d7e1543498c12a928cb92b66dc9f7
Amit Vishwakarma <amit200698@gmail.com>
2023-05-29 20:47:46 +0530

    configured the handleinputkeypress function

--- todo.js
+++ todo.js
@@ -2,7 +2,6 @@ const tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
-alert("hello")
 
 function renderList(){}
 
@@ -10,6 +9,31 @@ function markTaskAsComplete(taskId){}
 
 function deleteTask(taskId){}
 
-function addTask(task){}
+function addTask(task){
+}
 
-function showNotification(text){}
+function showNotification(text){
+    alert(text);
+}
+
+function handleInputKeypress(e){
+    if (e.key == 'Enter'){
+        const text = e.target.value;
+        console.log('text', text)
+
+        if(!text){
+            showNotification('Task text can not be empty');
+            return;
+        }
+
+        const task = {
+            text,
+            id: Date.now().toString(),
+            done: false
+        }
+        e.target.value = '';
+        addTask(task)
+    }
+}
+
+addTaskInput.addEventListener('keyup', handleInputKeypress);
